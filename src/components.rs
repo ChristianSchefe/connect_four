d9d1f1fcd3064b442503d7456c30d907b8fc62f1
Christian Schefe <schefe.c@eppendorf.de>
2023-11-13 14:33:37 +0100

    feat: add board, tiles, and tweening

--- src/components.rs
+++ src/components.rs
@@ -1,19 +1,62 @@
-use bevy::{math::U64Vec2, prelude::*};
+use bevy::prelude::*;
 
 use crate::GameState;
 
 #[derive(Resource)]
 pub struct Board {
-    pub size: U64Vec2,
+    pub size: UVec2,
     pub grid: Vec<Option<Player>>,
 }
 
 #[derive(Resource)]
 pub struct GameStateMachine(pub GameState);
 
-
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Player {
     PlayerOne,
-    PlaYerTwo,
+    PlayerTwo,
+}
+
+impl Board {
+    pub fn grid_to_world(self: &Board, grid_pos: UVec2) -> Vec2 {
+        grid_pos.as_vec2() - (self.size - UVec2::ONE).as_vec2() * 0.5
+    }
+
+    pub fn world_to_grid(self: &Board, world_pos: Vec2) -> Option<UVec2> {
+        let pos = (world_pos + (self.size - UVec2::ONE).as_vec2() * 0.5)
+            .round()
+            .as_ivec2();
+        if self.valid_ivec_pos(pos) {
+            Some(pos.as_uvec2())
+        } else {
+            None
+        }
+    }
+
+    pub fn valid_ivec_pos(self: &Board, pos: IVec2) -> bool {
+        pos.x >= 0 && pos.y >= 0 && (pos.x as u32) < self.size.x && (pos.y as u32) < self.size.y
+    }
+
+    pub fn valid_uvec_pos(self: &Board, pos: UVec2) -> bool {
+        pos.x < self.size.x && pos.y < self.size.y
+    }
+
+    pub fn set(self: &mut Board, grid_pos: UVec2, value: Option<Player>) {
+        let index = (grid_pos.x + grid_pos.y * self.size.x) as usize;
+        self.grid[index] = value;
+
+        debug!("Set {}({}) to {:?}", grid_pos, index, value);
+    }
+
+    pub fn get(self: &Board, grid_pos: UVec2) -> Option<Player> {
+        let index = (grid_pos.x + grid_pos.y * self.size.x) as usize;
+
+        if self.valid_uvec_pos(grid_pos) {
+            debug!("Get {}({}) -> {:?}", grid_pos, index, self.grid[index]);
+            self.grid[index]
+        } else {
+            debug!("Get {}({}) -> Invalid Position", grid_pos, index);
+            None
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 mod components;
 
-use bevy::{math::U64Vec2, prelude::*, render::camera::ScalingMode, window::PrimaryWindow};
+use bevy::{prelude::*, render::camera::ScalingMode, window::PrimaryWindow};
+use bevy_tweening::{
+    component_animator_system, lens::SpriteColorLens, AnimationSystem, Animator, EaseFunction,
+    Tween, TweeningPlugin,
+};
 use components::{Board, GameStateMachine};
+
+use crate::components::Player;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 
@@ -18,75 +24,152 @@ struct EndTurnEvent(pub GameState);
 #[derive(Resource, Default)]
 struct WorldCoords(Vec2);
 
+#[derive(Component, Debug)]
+struct GridPosition(UVec2);
+
 #[derive(Component)]
 struct MainCamera;
+
+#[derive(Component)]
+struct AnimationTarget(Option<Player>);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(TweeningPlugin)
         .insert_resource(ClearColor(BACKGROUND_COLOR))
-        .add_systems(Startup, setup)
+        .insert_resource(create_board_resource())
+        .add_systems(
+            Update,
+            component_animator_system::<Sprite>.in_set(AnimationSystem::AnimationUpdate),
+        )
+        .add_systems(Startup, (setup_camera, setup_board))
         .add_systems(Update, bevy::window::close_on_esc)
-        .add_systems(Update, (do_move, end_turn, calc_world_mouse))
+        .add_systems(Update, (do_move, end_turn, calc_world_mouse, update_tile))
         .insert_resource(GameStateMachine(GameState::PlayerOneTurn))
+        .init_resource::<WorldCoords>()
         .add_event::<EndTurnEvent>()
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    let size = U64Vec2 { x: 7, y: 6 };
-    commands.insert_resource(Board {
+fn create_board_resource() -> Board {
+    let size = UVec2 { x: 7, y: 6 };
+    let board = Board {
         size,
         grid: vec![None; (size.x * size.y) as usize],
-    });
-    commands.init_resource::<WorldCoords>();
+    };
+    board
+}
 
+fn setup_camera(mut commands: Commands) {
     let mut cam = Camera2dBundle::default();
     cam.projection.scaling_mode = ScalingMode::FixedVertical(10f32);
 
     commands.spawn((cam, MainCamera));
+}
 
-    commands.spawn((SpriteBundle {
-        transform: Transform {
-            translation: Vec3::new(0.0, 0.0, 0.0),
-            scale: Vec3::new(1.0, 1.0, 1.0),
-            ..default()
-        },
-        sprite: Sprite {
-            color: Color::rgb(1.0, 1.0, 1.0),
-            ..default()
-        },
-        ..default()
-    },));
+fn setup_board(mut commands: Commands, board: Res<Board>) {
+    for y in 0..board.size.y {
+        for x in 0..board.size.x {
+            let pos = UVec2 { x, y };
+            commands.spawn((
+                SpriteBundle {
+                    transform: Transform {
+                        translation: board.grid_to_world(pos).extend(0.0),
+                        scale: Vec3::new(0.9, 0.9, 1.0),
+                        ..default()
+                    },
+                    sprite: Sprite {
+                        color: Color::rgb(1.0, 1.0, 1.0),
+                        ..default()
+                    },
+                    ..default()
+                },
+                GridPosition(pos),
+                AnimationTarget(None),
+            ));
+        }
+    }
 }
 
 fn calc_world_mouse(
-    mut mycoords: ResMut<WorldCoords>,
-    q_window: Query<&Window, With<PrimaryWindow>>,
-    q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+    mut world_coords: ResMut<WorldCoords>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    cam_query: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
 ) {
-    let (camera, camera_transform) = q_camera.single();
-    let window = q_window.single();
+    let (camera, camera_transform) = cam_query.single();
+    let window = window_query.single();
 
     if let Some(world_position) = window
         .cursor_position()
         .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
         .map(|ray| ray.origin.truncate())
     {
-        mycoords.0 = world_position;
-        info!("World coords: {}/{}", world_position.x, world_position.y);
+        world_coords.0 = world_position;
     }
 }
 
 fn do_move(
     input: Res<Input<MouseButton>>,
     game_state: Res<GameStateMachine>,
-    board: Res<Board>,
+    mut board: ResMut<Board>,
+    mouse_position: Res<WorldCoords>,
     mut ev_end_turn: EventWriter<EndTurnEvent>,
 ) {
     if input.just_released(MouseButton::Left) {
-        ev_end_turn.send(EndTurnEvent(game_state.0));
-        debug!("End Turn Event sent");
+        info!(
+            "World coords: {}/{}",
+            mouse_position.0.x, mouse_position.0.y
+        );
+        if let Some(grid_pos) = board.world_to_grid(mouse_position.0) {
+            info!("Grid pos: {}/{}", grid_pos.x, grid_pos.y);
+            debug!("End Turn Event sent");
+
+            let cur_player = match &game_state.0 {
+                GameState::PlayerOneTurn => Some(Player::PlayerOne),
+                GameState::PlayerTwoTurn => Some(Player::PlayerTwo),
+                GameState::GameOver => None,
+            };
+
+            if board.get(grid_pos).is_none()
+                && (grid_pos.y == 0 || board.get(grid_pos - UVec2::new(0, 1)).is_some())
+            {
+                board.set(grid_pos, cur_player);
+                ev_end_turn.send(EndTurnEvent(game_state.0));
+            }
+            info!("Can't place here");
+        }
+    }
+}
+
+fn update_tile(
+    mut commands: Commands,
+    mut tiles: Query<(&GridPosition, &Sprite, &mut AnimationTarget, Entity)>,
+    board: Res<Board>,
+) {
+    for (pos, sprite, mut animation_target, entity) in tiles.iter_mut() {
+        let tile_type = board.get(pos.0);
+        let end_color = match tile_type {
+            Some(Player::PlayerOne) => Color::BLUE,
+            Some(Player::PlayerTwo) => Color::RED,
+            None => Color::WHITE,
+        };
+        if tile_type == animation_target.0 {
+            continue;
+        }
+        animation_target.0 = tile_type;
+
+        let tween = Tween::new(
+            EaseFunction::CubicInOut,
+            std::time::Duration::from_secs(1),
+            SpriteColorLens {
+                start: sprite.color,
+                end: end_color,
+            },
+        );
+        info!("Add animator at {:?}", pos);
+        commands.entity(entity).remove::<Animator<Sprite>>();
+        commands.entity(entity).insert(Animator::new(tween));
     }
 }
 
